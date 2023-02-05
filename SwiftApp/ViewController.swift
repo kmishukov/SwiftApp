04d11dcca8324e5d3025598ffa056cb5c62528b3
Konstantin <kkmishukov@gmail.com>
2023-02-05 22:37:47 +0400

    Initial Commit

---
+++ SwiftApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftApp
+//
+//  Created by Konstantin Mishukov on 05.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
